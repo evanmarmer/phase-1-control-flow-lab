f8dc6096c471244a51d54a7618751a7157883296
evanmarmer <evanmarmer@gmail.com>
2023-02-24 14:53:48 -0700

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let a
+  if (distance <= 400) {
+    a = 'This one is on me!'
+  } if (distance > 400 && distance <= 2000) {
+    a = 'That will be twenty bucks.'
+  } if (distance > 2000 && distance <= 2500) {
+    a = 'I will gladly take your thirty bucks.'
+  } if (distance > 2500) {
+    a = 'No can do.'
+  }
+
+  return a
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.'
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  
+  switch (tip){
+    case 'generous' :
+      return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+    
+
+  }
+
+
 }
